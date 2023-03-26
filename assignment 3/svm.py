8831f93fa63ee08643400c14c8c0b286cdb97cd8
Benjamin Ung <Benjaminung@cpp.edu>
2023-03-26 14:36:31 -0700

    still debugging ;-;

--- assignment 3/svm.py
+++ assignment 3/svm.py
@@ -30,7 +30,8 @@ X_test = np.array(df.values)[:,:64] #getting the first 64 fields to create the f
 y_test = np.array(df.values)[:,-1] #getting the last field to create the class testing data and convert them to NumPy array
 
 #created 4 nested for loops that will iterate through the values of c, degree, kernel, and decision_function_shape
-#--> add your Python code here
+best = 0
+best_comb = {}
 
 for i in c:
     for j in degree:
@@ -39,7 +40,7 @@ for i in c:
                 pass
                 #Create an SVM classifier that will test all combinations of c, degree, kernel, and decision_function_shape.
                 #For instance svm.SVC(c=1, degree=1, kernel="linear", decision_function_shape = "ovo")
-                clf = svm.SVC()
+                clf = svm.SVC(C=i,degree=j,kernel=k,decision_function_shape=d)
 
                 #Fit SVM to the training data
                 clf = clf.fit(X_training, y_training)
@@ -48,12 +49,15 @@ for i in c:
                 #hint: to iterate over two collections simultaneously, use zip()
                 #Example. for (x_testSample, y_testSample) in zip(X_test, y_test):
                 #to make a prediction do: clf.predict([x_testSample])
-                x_testSample = zip(X_test,y_test)
-                clf.predict([x_testSample])
-                print("done")
+                X_testSample = list(zip(X_test,y_test))
+                accuracy = clf.predict([X_test])
+                
                 #check if the calculated accuracy is higher than the previously one calculated. If so, update the highest accuracy and print it together
                 #with the SVM hyperparameters. Example: "Highest SVM accuracy so far: 0.92, Parameters: a=1, degree=2, kernel= poly, decision_function_shape = 'ovo'"
-                #--> add your Python code here
+                if(accuracy > best):
+                    best = accuracy
+                    best_comb = {i,j,k,d}
+                print(f"Highest SVM accuracy so far: {best}, Parameters: a={best_comb[0]}, degree={best_comb[1]},kernel={best_comb[2]},decision function shape={best_comb[3]}" )
 
 
 
